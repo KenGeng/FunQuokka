46d28619e22fb27930d632995403948120637332
KenGeng <samuelgeng7@gmail.com>
2024-02-17 22:22:19 +0800

    happy new year

--- src/main/java/org/funquokka/Main.java
+++ src/main/java/org/funquokka/Main.java
@@ -2,6 +2,6 @@ package org.funquokka;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello FunQuokka!");
+        System.out.println("Hello FunQuokka, happy new year!");
     }
 }
